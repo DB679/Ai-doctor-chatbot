53353470e6753507df9782b02fbc9590842bdcf3
DB679 <devbhatt16@yahoo.com>
2025-07-02 00:33:03 +0530

    Add files via upload

--- gradio_app.py
+++ gradio_app.py
@@ -100,7 +100,6 @@ with gr.Blocks(title="AI Doctor") as demo:
     )
 
 if __name__ == "__main__":
-    import gradio as gr
-    # your launch code like:
-    gr.Interface(...).launch(server_name="0.0.0.0", server_port=8080)
+    demo.launch(server_name="0.0.0.0", server_port=8080)
+
 
